e585bb7e9f79578cbbd1b99da8b10b2d261457de
zacktagnan <zacktagnan@gmail.com>
2025-01-27 19:46:49 +0100

    ItemCollection-Modelo.y.migraciOn-UserSeeder

---
+++ database/migrations/2025_01_27_190212_create_item_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_collections', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_collections');
+    }
+};
