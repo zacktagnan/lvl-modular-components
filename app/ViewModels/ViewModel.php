cc80c92eb724bcfa7fb6277016defba2cd5b4d4f
zacktagnan <zacktagnan@gmail.com>
2025-01-28 12:31:32 +0100

    ViewModel-Base

--- app/ViewModels/ViewModel.php
+++ app/ViewModels/ViewModel.php
@@ -2,7 +2,21 @@
 
 namespace App\ViewModels;
 
-class ViewModel
+use Reflection;
+use ReflectionClass;
+use ReflectionMethod;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Support\Arrayable;
+
+abstract class ViewModel implements Arrayable
 {
-    // Add your ViewModel logic here
+    public function toArray(): array
+    {
+        return collect((new ReflectionClass($this))
+            ->getMethods())
+            ->reject(fn (ReflectionMethod $method) => in_array($method->getName(), ['__construct', 'toArray']))
+            ->filter(fn (ReflectionMethod $method) => in_array('public', Reflection::getModifierNames($method->getModifiers())))
+            ->mapWithKeys(fn (ReflectionMethod $method) => [Str::camel($method->getName()) => $this->{$method->getName()}()])
+            ->toArray();
+    }
 }
