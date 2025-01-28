7b9414d0328a0a0f7a5e8b28889b86c299e964e3
zacktagnan <zacktagnan@gmail.com>
2025-01-28 18:19:23 +0100

    Base.User-User.adaptado.y.relaciones

--- app/Models/ItemCollection.php
+++ app/Models/ItemCollection.php
@@ -2,7 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Builders\ItemCollectionBuilder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class ItemCollection extends Base\ItemCollection
 {
-    //
+    public function newEloquentBuilder($query): ItemCollectionBuilder
+    {
+        return new ItemCollectionBuilder($query);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Authenticatable
+class User extends Base\User
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function itemCollections(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(ItemCollection::class);
     }
 }
