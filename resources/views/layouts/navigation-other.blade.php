400870318e3970416a3caac60157dff18150e676
zacktagnan <zacktagnan@gmail.com>
2025-02-03 23:39:19 +0100

    Ruta.HOME.y.enlace.a.Searchers.desde.Panel

--- resources/views/layouts/navigation-other.blade.php
+++ resources/views/layouts/navigation-other.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" class="size-14">
+                    <a href="{{ route('home') }}" class="size-14">
                         <x-application-logo class="block fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" class="size-14">
+                    <a href="{{ route('home') }}" class="size-14">
                         <x-application-logo class="block fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
@@ -14,6 +14,9 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('searchers.index')" :active="request()->routeIs('searchers.index')">
+                        {{ __('Buscadores') }}
                     </x-nav-link>
                 </div>
             </div>
