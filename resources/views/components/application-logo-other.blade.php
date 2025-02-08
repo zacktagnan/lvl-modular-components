d6b3b6d9a98410c3302558a4c0eca15186064ecd
zacktagnan <zacktagnan@gmail.com>
2025-02-08 01:58:43 +0100

    Cambio.de.favicon.y.logo.para.searchers

---
+++ resources/views/components/application-logo-other.blade.php
@@ -0,0 +1,4 @@
+
+<div class="p-2 bg-[#95afc0] rounded-md">
+    <img src="/img/searchers-logo.png" alt="Searchers-Logo">
+</div>
--- resources/views/layouts/navigation-other.blade.php
+++ resources/views/layouts/navigation-other.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}" class="size-14">
-                        <x-application-logo class="block fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo-other class="block fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
