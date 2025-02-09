6953c9f7ab77ca5b83ac97cf012a69aaefc43d70
zacktagnan <zacktagnan@gmail.com>
2025-02-09 23:41:13 +0100

    PiP.2d2-Filter.base

--- app/Livewire/Shop/Filters/Filter.php
+++ app/Livewire/Shop/Filters/Filter.php
@@ -2,13 +2,33 @@
 
 namespace App\Livewire\Shop\Filters;
 
-use Illuminate\View\View;
 use Livewire\Component;
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+use App\Livewire\Shop\Lists\ProductList;
+use Illuminate\Database\Eloquent\Collection;
 
 class Filter extends Component
 {
+    public string $title;
+
+    protected string $eloquentModel;
+
+    public function filters(): Collection
+    {
+        return collect();
+    }
+
+    public function applyFilters(array $filters): void
+    {
+        $this->dispatch('filters-updated', filters: $filters)->to(ProductList::class);
+    }
+
     public function render(): View
     {
-        return view('livewire.shop.filters.filter');
+        return view('livewire.shop.filters.filter', [
+            'filters' => $this->filters(),
+            'alias' => Str::of(class_basename($this->eloquentModel))->lower(),
+        ]);
     }
 }
--- resources/views/livewire/shop/filters/filter.blade.php
+++ resources/views/livewire/shop/filters/filter.blade.php
@@ -1,3 +1,27 @@
-<div>
-    {{-- Be like water. --}}
-</div>
+<x-filter-card :title="$title">
+    <ul class="list-none border-0">
+        @foreach ($filters as $filter)
+            <li
+                class="px-4 py-2 border-b border-gray-200"
+                wire:key="{{ $alias }}-filter-{{ $filter->id }}"
+            >
+                <input
+                    class="form-checkbox me-1"
+                    type="checkbox"
+                    value="{{ $filter->id }}"
+                    id="{{ $alias }}-filter-{{ $filter->id }}"
+                    wire:model.live="selectedFilters"
+                />
+                <label
+                    class="ml-2"
+                    for="{{ alias }}-filter-{{ $filter->id }}"
+                >
+                    {{ $filter->name }}
+                </label>
+                <span class="inline-block px-2 py-1 text-xs font-semibold text-white bg-blue-500 rounded">
+                    {{ $filter->products_count }}
+                </span>
+            </li>
+        @endforeach
+    </ul>
+</x-filter-card>
