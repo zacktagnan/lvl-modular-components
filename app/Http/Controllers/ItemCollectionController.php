9476c5da1e89194ba8928bcb44bfbf69fc4c6d7b
zacktagnan <zacktagnan@gmail.com>
2025-02-20 19:42:08 +0100

    ItemCollection-Aplicar.las.polIticas.de.acceso

--- app/Http/Controllers/ItemCollectionController.php
+++ app/Http/Controllers/ItemCollectionController.php
@@ -2,20 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+use App\Models\ItemCollection;
+use Illuminate\Support\Facades\Gate;
 use App\ViewModels\ItemCollections\GetItemCollectionViewModel;
 use App\ViewModels\ItemCollections\UpsertItemCollectionViewModel;
-use Illuminate\View\View;
 
 class ItemCollectionController extends Controller
 {
     public function index(GetItemCollectionViewModel $viewModel): View
     {
+        Gate::authorize('viewAny', ItemCollection::class);
+
         // dd($viewModel->toArray());
         return view('collections.index', $viewModel);
     }
 
     public function create(): View
     {
+        Gate::authorize('create', ItemCollection::class);
+
         $viewModel = app(UpsertItemCollectionViewModel::class, [
             'itemCollection' => null,
         ]);
