4c75245f5e8d590effbc01009b2ed3f898bca2d7
zacktagnan <zacktagnan@gmail.com>
2025-02-20 18:54:01 +0100

    ItemCollection-DTO

---
+++ app/DTOs/ItemCollectionDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DTOs;
+
+use Illuminate\Support\Str;
+use App\Http\Requests\UpsertItemCollectionRequest;
+
+readonly class ItemCollectionDTO
+{
+    private function __construct(public string $name, public string $slug, public string $description)
+    {}
+
+    public static function fromRequest(UpsertItemCollectionRequest $request): self
+    {
+        return new self(
+            name: $request->validated('name'),
+            slug: Str::slug($request->validated('slug')),
+            description: $request->validated('description'),
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+        ];
+    }
+}
